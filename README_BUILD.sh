b24d0dd9da8b07807036807e27f4e02eb35253ee
Masao Miura <masao.miura@gunjou.co.jp>
2014-12-30 09:45:11 +0900

    Append comment about build instructions on Windows

--- README_BUILD.sh
+++ README_BUILD.sh
@@ -1,4 +1,7 @@
 # Gets depot_tools.
+# http://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools_tutorial.html#_setting_up
+# On Windows, Check below page.
+# http://www.chromium.org/developers/how-tos/build-instructions-windows
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools
 export PATH=$PATH:/path_to_parent_dir/depot_tools
 # Configures git.
